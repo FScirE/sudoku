4d1185b2bbfe8047fd3a330b4a2ef01d74041995
FScirE <leo.soleke@gmail.com>
2025-08-06 00:26:21 +0200

    fixed build

--- build.ps1
+++ build.ps1
@@ -2,4 +2,6 @@ Write-Host "Building Sudoku Executable"
 Remove-Item -Path "./build" -Recurse
 Remove-Item -Path "./dist" -Recurse
 Remove-Item -Path "./sudoku.spec"
-py -m PyInstaller ./game.py -n sudoku -w -F --clean
+py -m PyInstaller ./game.py -n sudoku -w -F
+Copy-Item -Path "./arial" -Destination "./dist/" -Recurse
+Write-Host "Sudoku Executable Created"
